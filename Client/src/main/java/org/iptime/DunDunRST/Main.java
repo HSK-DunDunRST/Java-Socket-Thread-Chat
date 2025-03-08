f6762f70bc4ab1b3a589783c065f83ad8aeb89f3
HSK_DunDunRST <118043971+HSK-DunDunRST@users.noreply.github.com>
2025-03-08 13:13:36 +0900

    Update Main.java

--- Client/src/main/java/org/iptime/DunDunRST/Main.java
+++ Client/src/main/java/org/iptime/DunDunRST/Main.java
@@ -6,7 +6,7 @@ import java.net.Socket;
 public class Main {
 
     public static void main(String[] args) throws IOException {
-        Socket socket = new Socket("localhost", 25252); // Server IP와 PortNumber는 직접 설정하셔야됩니다.
+        Socket socket = new Socket("localhost", 3000); // Server IP와 PortNumber는 직접 설정하셔야됩니다.
         System.out.println("서버와 연결되었습니다.");
         ReadThread th1 = new ReadThread(socket);
         WriteThread th2 = new WriteThread(socket);
